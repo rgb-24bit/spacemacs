05728c6a74ced953e5aa3575ab8b82a51112099b
rgb-24bit <rgb-24bit@foxmail.com>
2020-10-10 11:20:57 +0800

    feat: add spacemacs-private distribution

---
+++ layers/+distributions/spacemacs-private/layers.el
@@ -0,0 +1,20 @@
+;;; layers.el --- spacemacs-private distribution layers file.
+
+;; We use `spacemacs/declare-layers' instead of
+;; `configuration-layer/declare-layer-dependencies' in order to declare
+;; the layers right away
+;; This is a special case only for distribution layers.
+(configuration-layer/declare-layers
+ '(
+   spacemacs-completion
+   spacemacs-defaults
+   spacemacs-editing
+   spacemacs-editing-visual
+   spacemacs-modeline
+   spacemacs-navigation
+   spacemacs-org
+   spacemacs-project
+   spacemacs-visual
+   ))
+
+;;; layers.el ends here
